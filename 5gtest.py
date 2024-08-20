c77228b8c91b41983e440bbd0984e12d6af30d42
Ruan Batista Rodrigues <145821191+zzruanxx@users.noreply.github.com>
2024-08-20 00:15:21 -0300

    Add files via upload
    treino de rotina - python

---
+++ 5gtest.py
@@ -0,0 +1,19 @@
+import math
+
+#raiz
+raiz_quadrada = math.sqrt(16)
+
+#fatorial
+fatorial = math.factorial(5)
+
+#seno, cosseno, tangente
+seno = math.sin(math.pi / 2)
+cosseno = math.cos(math.pi)
+tangente = math.tan(math.pi / 4)
+
+log_natural = math.log(10)
+
+log_base10 = math.log10(100)
+
+print(raiz_quadrada, fatorial, seno, cosseno, tangente, log_natural, log_base10)
+
